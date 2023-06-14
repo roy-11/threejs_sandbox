f6a7129dcec483c0d45195a06b95c66f4c618fc4
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-06-15 03:26:20 +0900

    add positon, scale, rotaiton, group, axesHelper, etc

--- src/script.js
+++ src/script.js
@@ -5,11 +5,67 @@ console.log(THREE);
 // Scene
 const scene = new THREE.Scene();
 
+// Objects
+const group = new THREE.Group();
+group.scale.y = 2;
+group.rotation.y = 0.2;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -1.5;
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = 0;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 1.5;
+group.add(cube3);
+
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// Position
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
+
+// Scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+// mesh.rotation.x = Math.PI;
+// mesh.rotation.x = Math.PI * 0.5;
+// mesh.rotation.x = Math.PI / 2;
+mesh.rotation.reorder("YXZ");
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+
+// Others
+// console.log("length:", mesh.position.length());
+// console.log("distance:", mesh.position.distanceTo(new THREE.Vector3(0, 1, 2)));
+// mesh.position.normalize();
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
 
 // Size
 const sizes = {
@@ -20,9 +76,8 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-camera.position.y = 1;
-camera.position.x = 1;
 scene.add(camera);
+camera.lookAt(mesh.position);
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
