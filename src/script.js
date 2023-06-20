eabec75f9fcd60c43e737420c123a3f4b5c21111
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-06-20 23:03:08 +0900

    add perspective camera animation by cursor and controls

--- src/script.js
+++ src/script.js
@@ -1,37 +1,8 @@
 import * as THREE from "three";
-import gsap from "gsap";
-
-console.log(THREE);
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // Scene
 const scene = new THREE.Scene();
-
-// Objects
-const group = new THREE.Group();
-group.scale.y = 2;
-group.rotation.y = 0.2;
-scene.add(group);
-
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube1.position.x = -1.5;
-group.add(cube1);
-
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-);
-cube2.position.x = 0;
-group.add(cube2);
-
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x0000ff })
-);
-cube3.position.x = 1.5;
-group.add(cube3);
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -39,49 +10,64 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// Position
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1);
-
-// Scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
-
-// Rotation
-// mesh.rotation.x = Math.PI;
-// mesh.rotation.x = Math.PI * 0.5;
-// mesh.rotation.x = Math.PI / 2;
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
-
-// Others
-// console.log("length:", mesh.position.length());
-// console.log("distance:", mesh.position.distanceTo(new THREE.Vector3(0, 1, 2)));
-// mesh.position.normalize();
-
-// Axes helper
-const axesHelper = new THREE.AxesHelper(3);
-scene.add(axesHelper);
-
 // Size
 const sizes = {
   width: 800,
   height: 600,
 };
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  console.log(cursor.x, cursor.y);
+
+  if (event.clientX >= 0 && event.clientX <= sizes.width) {
+    cursor.x = event.clientX / sizes.width - 0.5; // -0.5 ~ 0.5
+  }
+  if (event.clientY >= 0 && event.clientY <= sizes.height) {
+    cursor.y = -(event.clientY / sizes.height - 0.5); // -0.5 ~ 0.5
+  }
+});
+
+// PerspectiveCamera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  100
+);
+
+// OrthographicCamera
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+
+// position
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 2;
+
 scene.add(camera);
 camera.lookAt(mesh.position);
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.target.y = 1;
+// controls.update();
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -90,41 +76,23 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-/**
- * Animate
- */
-
-// let time = Date.now();
-const clock = new THREE.Clock();
-
-// Update objects By gsap
-// gsap.to(mesh.position, { duration: 100, delay: 1, x: 200 });
-
+// Animation
 const tick = () => {
-  // Update objects By Frame
-  // mesh.rotation.y += 0.01;
+  // Update camera by cursor
+  // camera.position.x = cursor.x * 3;
+  // camera.position.y = cursor.y * 3;
+  // console.log("cursor.x", cursor.x * Math.PI * 2); // -3.14 ~ 3.14
+  // console.log("sin:", Math.sin(cursor.x * Math.PI * 2)); // 0 → 1 → 0 → -1 → 0
+  // console.log("cos:", Math.cos(cursor.x * Math.PI * 2)); // -1 → 0 → 1 → 0 → -1
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position);
 
-  // Update objects By Time
-  // const currentTime = Date.now();
-  // const deltaTime = currentTime - time;
-  // time = currentTime;
-  // mesh.rotation.y += 0.001 * deltaTime;
+  // Update controls
+  controls.update();
 
-  // Update objects By Clock
-  const elapsedTime = clock.getElapsedTime();
-  mesh.rotation.y = elapsedTime;
-  mesh.position.x = Math.cos(elapsedTime);
-  mesh.position.y = Math.sin(elapsedTime);
-
-  // Update camera By Clock
-  camera.position.x = Math.cos(elapsedTime);
-  camera.position.y = Math.sin(elapsedTime);
-  camera.lookAt(mesh.position);
-
-  // Render
   renderer.render(scene, camera);
-
-  // Call tick again on the next frame
   window.requestAnimationFrame(tick);
 };
 
