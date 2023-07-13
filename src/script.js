c512113ec336024ac0c16c5b50da9c694cf9af7d
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-07-13 21:00:44 +0900

    comment out 15_lights code

--- src/script.js
+++ src/script.js
@@ -243,26 +243,62 @@ const axisHelper = new THREE.AxesHelper();
 // torus.position.x = 1.5;
 // scene.add(sphere, plane, torus);
 
-// Object 3
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
+scene.add(ambientLight);
+
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(2, 2, -1);
+gui.add(directionalLight, "intensity").min(0).max(1).step(0.001);
+gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001);
+scene.add(directionalLight);
+
+/**
+ * Materials
+ */
 const material = new THREE.MeshStandardMaterial();
-material.roughness = 0.4;
+material.roughness = 0.7;
+gui.add(material, "metalness").min(0).max(1).step(0.001);
+gui.add(material, "roughness").min(0).max(1).step(0.001);
 
+/**
+ * Objects
+ */
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
-sphere.position.x = -1.5;
-
-const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
-
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-  material
-);
-torus.position.x = 1.5;
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
 plane.rotation.x = -Math.PI * 0.5;
-plane.position.y = -0.65;
+plane.position.y = -0.5;
 
-scene.add(sphere, cube, torus, plane);
+scene.add(sphere, plane);
+
+// Object 3
+// const material = new THREE.MeshStandardMaterial();
+// material.roughness = 0.4;
+//
+// const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
+// sphere.position.x = -1.5;
+//
+// const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
+//
+// const torus = new THREE.Mesh(
+//   new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+//   material
+// );
+// torus.position.x = 1.5;
+//
+// const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+// plane.rotation.x = -Math.PI * 0.5;
+// plane.position.y = -0.65;
+//
+// scene.add(sphere, cube, torus, plane);
 
 // Lights 1
 // const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
@@ -275,68 +311,68 @@ scene.add(sphere, cube, torus, plane);
 // scene.add(pointLight);
 
 // Lights 2
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-gui
-  .add(ambientLight, "intensity")
-  .min(0)
-  .max(1)
-  .step(0.0001)
-  .name("Ambient Light intensity");
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
-directionalLight.position.set(1, 0.25, 0);
-gui
-  .add(directionalLight.position, "x")
-  .min(0)
-  .max(1)
-  .step(0.0001)
-  .name("Directional Light x");
-scene.add(directionalLight);
-
-const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
-scene.add(hemisphereLight);
-
-const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
-pointLight.position.set(1, -0.5, 1);
-scene.add(pointLight);
-
-const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
-rectAreaLight.position.set(1.5, -0, -1.5);
-rectAreaLight.lookAt(new THREE.Vector3());
-scene.add(rectAreaLight);
-
-const spotLight = new THREE.SpotLight(
-  0x78ff00,
-  0.5,
-  10,
-  Math.PI * 0.1,
-  0.25,
-  1
-);
-spotLight.position.set(0, 2, 3);
-scene.add(spotLight);
-
-const hemisphereLightHelper = new THREE.HemisphereLightHelper(
-  hemisphereLight,
-  0.2
-);
-scene.add(hemisphereLightHelper);
-
-const directionalLightHelper = new THREE.DirectionalLightHelper(
-  directionalLight,
-  0.2
-);
-scene.add(directionalLightHelper);
-
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
-scene.add(pointLightHelper);
-
-const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-scene.add(spotLightHelper);
-
-const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
-scene.add(rectAreaLightHelper);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// gui
+//   .add(ambientLight, "intensity")
+//   .min(0)
+//   .max(1)
+//   .step(0.0001)
+//   .name("Ambient Light intensity");
+// scene.add(ambientLight);
+//
+// const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
+// directionalLight.position.set(1, 0.25, 0);
+// gui
+//   .add(directionalLight.position, "x")
+//   .min(0)
+//   .max(1)
+//   .step(0.0001)
+//   .name("Directional Light x");
+// scene.add(directionalLight);
+//
+// const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
+// scene.add(hemisphereLight);
+//
+// const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
+// pointLight.position.set(1, -0.5, 1);
+// scene.add(pointLight);
+//
+// const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
+// rectAreaLight.position.set(1.5, -0, -1.5);
+// rectAreaLight.lookAt(new THREE.Vector3());
+// scene.add(rectAreaLight);
+//
+// const spotLight = new THREE.SpotLight(
+//   0x78ff00,
+//   0.5,
+//   10,
+//   Math.PI * 0.1,
+//   0.25,
+//   1
+// );
+// spotLight.position.set(0, 2, 3);
+// scene.add(spotLight);
+//
+// const hemisphereLightHelper = new THREE.HemisphereLightHelper(
+//   hemisphereLight,
+//   0.2
+// );
+// scene.add(hemisphereLightHelper);
+//
+// const directionalLightHelper = new THREE.DirectionalLightHelper(
+//   directionalLight,
+//   0.2
+// );
+// scene.add(directionalLightHelper);
+//
+// const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+// scene.add(pointLightHelper);
+//
+// const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+// scene.add(spotLightHelper);
+//
+// const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight);
+// scene.add(rectAreaLightHelper);
 
 // const pointLight = new THREE.PointLight(0xffffff, 0.5);
 // pointLight.position.x = 2;
