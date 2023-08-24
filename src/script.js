90f000e702051c459c909d00e64c7d3106d5c24e
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-08-25 00:03:30 +0900

    add random color particles

--- src/script.js
+++ src/script.js
@@ -20,21 +20,24 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load("/textures/particles/2.png");
 
 /**
  * Particles
  */
 // const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
 const particlesGeometry = new THREE.BufferGeometry();
-const count = 500;
+const count = 500000;
 
 // Multiply by 3 because each position is composed of 3 values (x, y, z)
 const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
 
 // Multiply by 3 for same reason
 for (let i = 0; i < count * 3; i++) {
   // Math.random() - 0.5 to have a random value between -0.5 and +0.5
   positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
 }
 
 // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
@@ -42,11 +45,21 @@ particlesGeometry.setAttribute(
   "position",
   new THREE.BufferAttribute(positions, 3)
 );
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
 const particlesMaterial = new THREE.PointsMaterial({
-  size: 0.01,
+  size: 0.02,
   sizeAttenuation: true,
 });
+particlesMaterial.color = new THREE.Color("#ff88cc");
+// particlesMaterial.map = particleTexture;
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particleTexture;
+// particlesMaterial.alphaTest = 0.001;
+// particlesMaterial.depthTest = false;
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending;
+particlesMaterial.vertexColors = true;
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
