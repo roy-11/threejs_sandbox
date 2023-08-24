92d23af08657fcaf4e5c5071078cde67b10df657
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-08-24 23:43:28 +0900

    add random particles

--- src/script.js
+++ src/script.js
@@ -24,9 +24,27 @@ const textureLoader = new THREE.TextureLoader();
 /**
  * Particles
  */
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 500;
+
+// Multiply by 3 because each position is composed of 3 values (x, y, z)
+const positions = new Float32Array(count * 3);
+
+// Multiply by 3 for same reason
+for (let i = 0; i < count * 3; i++) {
+  // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+  positions[i] = (Math.random() - 0.5) * 10;
+}
+
+// Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+
 const particlesMaterial = new THREE.PointsMaterial({
-  size: 0.02,
+  size: 0.01,
   sizeAttenuation: true,
 });
 
