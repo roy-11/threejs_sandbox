7a4d4e211161d20db235f50c49070511c82f49ca
roy-11 <63440731+roy-11@users.noreply.github.com>
2023-11-26 01:19:22 +0900

    add base particles

--- src/script.js
+++ src/script.js
@@ -12,6 +12,41 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 const gui = new GUI();
 
+/**
+ * Galaxy
+ */
+const parameters = {};
+parameters.count = 1000;
+parameters.size = 0.02;
+
+const generateGalaxy = () => {
+  const geometry = new THREE.BufferGeometry();
+  const positions = new Float32Array(parameters.count * 3);
+
+  for (let i = 0; i < parameters.count; i++) {
+    const i3 = i * 3;
+
+    positions[i3] = (Math.random() - 0.5) * 3;
+    positions[i3 + 1] = (Math.random() - 0.5) * 3;
+    positions[i3 + 2] = (Math.random() - 0.5) * 3;
+  }
+
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+
+  // Material
+  const material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+  });
+
+  const points = new THREE.Points(geometry, material);
+  scene.add(points);
+};
+
+generateGalaxy();
+
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
