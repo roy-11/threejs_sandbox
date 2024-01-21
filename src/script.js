17a4936f526522f94a38c9a327f3b7e6ace2dea5
roy-11 <63440731+roy-11@users.noreply.github.com>
2024-01-21 20:43:18 +0900

    add cannon and base world

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import GUI from "lil-gui";
 // import gsap from "gsap";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import CANNON from "cannon";
 
 THREE.ColorManagement.enabled = false;
 
@@ -32,6 +33,27 @@ const environmentMapTexture = cubeTextureLoader.load(
   console.log,
   console.log,
 );
+
+// World
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+
+// Sphere
+const sphereShape = new CANNON.Sphere(0.5);
+const sphereBody = new CANNON.Body({
+  mass: 1,
+  position: new CANNON.Vec3(0, 3, 0),
+  shape: sphereShape,
+});
+world.addBody(sphereBody);
+
+// Floor
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+world.addBody(floorBody);
 
 /**
  * Test sphere
@@ -138,9 +160,21 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 const clock = new THREE.Clock();
+let oldElapsedTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
+
+  // Update physics
+  world.step(1 / 60, deltaTime, 3);
+
+  sphere.position.copy(sphereBody.position);
+  // sphere.position.x = sphereBody.position.x;
+  // sphere.position.y = sphereBody.position.y;
+  // sphere.position.z = sphereBody.position.z;
 
   // Update controls
   controls.update();
