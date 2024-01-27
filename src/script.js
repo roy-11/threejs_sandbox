f75f1738d84812874b063f28066634e8c2269bf9
roy-11 <63440731+roy-11@users.noreply.github.com>
2024-01-28 02:59:19 +0900

    feat: add createBox function

--- src/script.js
+++ src/script.js
@@ -181,7 +181,6 @@ const createSphere = (radius, position) => {
 
   objectsToUpdate.push({ mesh, body });
 };
-createSphere(0.5, { x: 0, y: 3, z: 0 });
 
 const debugObject = {};
 debugObject.createSphere = () => {
@@ -192,6 +191,53 @@ debugObject.createSphere = () => {
   });
 };
 gui.add(debugObject, "createSphere");
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 1,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+  envMapIntensity: 0.5,
+});
+const createBox = (width, height, depth, position) => {
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  mesh.scale.set(width, height, depth);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5),
+  );
+
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  world.addBody(body);
+
+  objectsToUpdate.push({ mesh, body });
+};
+
+createBox(1, 1.5, 2, { x: 0, y: 3, z: 0 });
+
+debugObject.createBox = () => {
+  createBox(
+    Math.random(), // width
+    Math.random(), // height
+    Math.random(), // depth
+    // random position
+    {
+      x: (Math.random() - 0.5) * 3,
+      y: 3,
+      z: (Math.random() - 0.5) * 3,
+    },
+  );
+};
+gui.add(debugObject, "createBox");
 
 /**
  * Animate
@@ -209,6 +255,7 @@ const tick = () => {
   world.step(1 / 60, deltaTime, 3);
   for (const object of objectsToUpdate) {
     object.mesh.position.copy(object.body.position);
+    object.mesh.quaternion.copy(object.body.quaternion);
   }
 
   // Update controls
